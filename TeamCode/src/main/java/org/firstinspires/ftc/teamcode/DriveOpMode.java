bae557566332928bb8ee8233ba65f76cffe7763c
Wumbo Laptop <mumuop500@gmail.com>
2026-08-27 16:32:38 -0600

    @TeleOp added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveOpMode.java
@@ -1,24 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Drive", group = "TeleOp")
 public class DriveOpMode extends OpMode {
 
     MecanumDrive drive = new MecanumDrive();
 
     double forward, strafe, rotate;
 
+    @Override
     public void init() {
         drive.init(hardwareMap);
     }
 
+    @Override
     public void loop() {
 
-        forward = gamepad1.left_stick_x;
+        driveLoop();
+
+    }
+
+    public void driveLoop() {
+        forward = gamepad1.left_stick_y;
         strafe = gamepad1.left_stick_x;
         rotate = gamepad1.right_stick_x;
 
         drive.driveFieldRelative(forward, strafe, rotate);
-
     }
-
 }
